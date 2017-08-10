9c5e6f76ae3db83c16141fcb771225057cdd9c78
zoombear <varchambault@victory2016.org>
2017-08-10 16:05:36 -0400

    initial commit

---
+++ compactness_calculator_dialog.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ compactnessCalculatorDialog
+                                 A QGIS plugin
+ Compactness calculations
+                             -------------------
+        begin                : 2017-08-10
+        git sha              : $Format:%H$
+        copyright            : (C) 2017 by Ariel and Vanessa
+        email                : vanessa@flippable.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+import os
+
+from PyQt4 import QtGui, uic
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'compactness_calculator_dialog_base.ui'))
+
+
+class compactnessCalculatorDialog(QtGui.QDialog, FORM_CLASS):
+    def __init__(self, parent=None):
+        """Constructor."""
+        super(compactnessCalculatorDialog, self).__init__(parent)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
